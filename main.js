0b9a7be9ea552aaf3439c8da177c63844c508f27
MargoKrk <malgorzata.walega89@gmail.com>
2023-08-05 22:58:54 +0200

    print filtred values

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import "./css/style.css";
-import { sortItems } from "./js/general";
 import { types$, categories$ } from "./js/base";
 
 document.querySelector("#app").innerHTML = `
@@ -30,35 +29,43 @@ Promise.all([types$, categories$]).then((values) => {
   });
   postTypes(values[0]);
   postCategory(values[1]);
-  console.log(values[0]);
-  console.log(values[1]);
+
+  const allButtons = document.querySelectorAll(".type-button");
+  const allCategoriesCard = document.querySelectorAll(".categoryCard");
+
+  allButtons.forEach((currentButton) => {
+    currentButton.addEventListener("click", function (e) {
+      currentButton.classList.toggle("selected");
+
+      let filterTarget = e.target.dataset.filter;
+
+      let newValue = values[1].filter((elem) => {
+        console.log(elem);
+
+        return elem.type == filterTarget;
+      });
+
+      console.log(newValue);
+    });
+  });
 });
 
 function postTypes(types) {
   types.forEach((type) => {
-    typesHTML = `<button class="type-button">${type.name}</button>`;
+    typesHTML = `<button class="type-button" data-filter="${type.id}">${type.name}</button>`;
     typesList.innerHTML += typesHTML;
   });
 }
 
 function postCategory(categories) {
   categories.forEach((category) => {
-    categoriesHTML = `<div class="categoryCard">
+    categoriesHTML = `<div class="categoryCard" data-filter="${category.type}">
     <img src="${category.iconUrl}" alt="${category.name}" width="64" height="64">
     <p>${category.name}</p>
     </div>`;
     categoriesList.innerHTML += categoriesHTML;
   });
 }
-
-// const allButtons = document.querySelectorAll(".type-button");
-// console.log(allButtons);
-
-// allButtons.forEach(function (currentButton) {
-//   console.log(currentButton);
-
-//   currentButton.addEventListener("click", function () {});
-// });
 
 // setupCounter(document.querySelector('#counter'))
 
